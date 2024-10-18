7dc6a096b06bf982904e821c310a95975bbced35
songant <18769796173@163.com>
2024-10-18 14:05:09 +0800

    安装文件修改测试：mac安装修改以及支持linux

--- Formula/ogc-cli@0.0.2.rb
+++ Formula/ogc-cli@0.0.2.rb
@@ -3,22 +3,42 @@ class OgcCliAT002 < Formula
   homepage "https://github.com/saibit-tech/open-gpu-cloud"
   version "0.0.2"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.2/ogc_cli-darwin_amd64"
-    sha256 "3c0b574a94bb4249aebb6ca903b4fd0516d7699886253b135859799576396b1d"
-  elsif Hardware::CPU.arm?
-    url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.2/ogc_cli-darwin_arm64"
-    sha256 "13dd2f8f19f02e3145165fce0de8e226f5dff4994c7779888238952b0b5a6b8e"
-  else
-    odie "Unsupported CPU architecture"
-  end
+  if OS.mac? && Hardware::CPU.intel?
+      url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.2/ogc_cli-darwin_amd64"
+      sha256 "3c0b574a94bb4249aebb6ca903b4fd0516d7699886253b135859799576396b1d"
+    end
+
+    if OS.mac? && Hardware::CPU.arm?
+       url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.2/ogc_cli-darwin_arm64"
+       sha256 "13dd2f8f19f02e3145165fce0de8e226f5dff4994c7779888238952b0b5a6b8e"
+    end
+
+    if OS.linux? && Hardware::CPU.intel?
+      url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.2/ogc_cli-linux_amd64"
+      sha256 "3c87a28cf98a439623cd41e92f46c2a8c7ff7f72875dde459f01fc2a03d8f3f4"
+    end
+
+    if OS.linux? && Hardware::CPU.arm?
+      url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.2/ogc_cli-linux_arm64"
+      sha256 "f2aa6eb954e3fc091f8b0d7f85bb9bd129a5da0193cf350a8cfe0baa32b2f2cf"
+    end
 
   def install
     if Hardware::CPU.intel?
       bin.install "ogc_cli-darwin_amd64" => "ogc"
-    elsif Hardware::CPU.arm?
+    end
+    if Hardware::CPU.arm?
       bin.install "ogc_cli-darwin_arm64" => "ogc"
     end
+
+    if OS.linux? && Hardware::CPU.intel?
+       bin.install "ogc_cli-linux_amd64" => "ogc"
+    end
+
+    if OS.linux? && Hardware::CPU.arm?
+       bin.install "ogc_cli-linux_arm64" => "ogc"
+    end
+
     system "xattr", "-c", "#{bin}/ogc"
   end
 
--- Formula/ogc-cli@0.0.3.rb
+++ Formula/ogc-cli@0.0.3.rb
@@ -3,22 +3,42 @@ class OgcCliAT003 < Formula
   homepage "https://github.com/saibit-tech/open-gpu-cloud"
   version "0.0.3"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.3/ogc_cli-darwin_amd64"
-    sha256 "340d392ba4968203325bb4e30eb890eb145e3aa2023cb9f416b9df9dd7558b77"
-  elsif Hardware::CPU.arm?
-    url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.3/ogc_cli-darwin_arm64"
-    sha256 "903db130e2aeeb0d4ec32e5df9378cc503eb0dc8b0a401073c8eb4f885de95d6"
-  else
-    odie "Unsupported CPU architecture"
-  end
+  if OS.mac? && Hardware::CPU.intel?
+      url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.3/ogc_cli-darwin_amd64"
+      sha256 "340d392ba4968203325bb4e30eb890eb145e3aa2023cb9f416b9df9dd7558b77"
+    end
+
+    if OS.mac? && Hardware::CPU.arm?
+       url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.3/ogc_cli-darwin_arm64"
+       sha256 "903db130e2aeeb0d4ec32e5df9378cc503eb0dc8b0a401073c8eb4f885de95d6"
+    end
+
+    if OS.linux? && Hardware::CPU.intel?
+      url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.3/ogc_cli-linux_amd64"
+      sha256 "30a0a3eb0f3d5ed78b5613ac9b1e26f144821877a91c1cc1d530edfbbfb345c9"
+    end
+
+    if OS.linux? && Hardware::CPU.arm?
+      url "https://github.com/saibit-tech/homebrew-ogc-cli/releases/download/ogc-cli-v0.0.3/ogc_cli-linux_arm64"
+      sha256 "92851dccd167938b2391d6644b0d22d052d319c06b23fca52435ccf51fa90f4d"
+    end
 
   def install
     if Hardware::CPU.intel?
       bin.install "ogc_cli-darwin_amd64" => "ogc"
-    elsif Hardware::CPU.arm?
+    end
+    if Hardware::CPU.arm?
       bin.install "ogc_cli-darwin_arm64" => "ogc"
     end
+
+    if OS.linux? && Hardware::CPU.intel?
+       bin.install "ogc_cli-linux_amd64" => "ogc"
+    end
+
+    if OS.linux? && Hardware::CPU.arm?
+       bin.install "ogc_cli-linux_arm64" => "ogc"
+    end
+
     system "xattr", "-c", "#{bin}/ogc"
   end
 
